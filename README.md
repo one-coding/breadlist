cc1ac91657757c8464300f4707ebc5d96ef0ede9
one-coding <75924046+one-coding@users.noreply.github.com>
2021-03-19 19:57:13 +0900

    03.19
    폴더 생성 및 네비게이션 바 생성

--- README.md
+++
@@ -1,2 +0,0 @@
-# breadlist
-breadlist project
---
+++ real-world-vue/src/components/Slider.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-container class="bv-example-row">
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="5000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="800"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=52">
+        <h1 class="slider-text">시네모 입니다</h1>
+      </b-carousel-slide>
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+    </b-carousel>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      slide: 0,
+      sliding: null,
+    }
+  },
+  methods: {
+    onSlideStart() {
+      this.sliding = true
+    },
+    onSlideEnd() {
+      this.sliding = false
+    },
+  },
+}
+</script>
+
+<style>
+.slider-text {
+  font-size: 20px;
+}
+</style>
